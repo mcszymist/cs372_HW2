4ae299babac29f42bcbbd9ca2566f750399e71df
Tyler Roberts <mcszymist7@gmail.com>
2018-03-29 18:01:20 -0800

    Rotation passing all test atm

---
+++ ComplexShapes.cpp
@@ -0,0 +1,11 @@
+#include <string>
+using std::string;
+#include <sstream>
+using std::stringstream;
+#include "ComplexShapes.h"
+
+string Rotated::getPostscript(){
+    stringstream ss;
+    ss << angle << " rotate " << shape->getPostscript();
+    return ss.str();
+}
