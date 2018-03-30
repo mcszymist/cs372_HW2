3a34c1465ca3da3ea80dcd016535f4d3933fa5ea
Tyler Roberts <mcszymist7@gmail.com>
2018-03-29 20:11:11 -0800

    Poping new file maker

--- cps.cpp
+++ cps.cpp
@@ -1,1 +1,12 @@
+#include <vector>
+using std::vector;
+#include "Shape.h"
+#include "SimpleShapes.h"
+#include "ComplexShapes.h"
+#include "UniqueShapes.h"
 
+
+int makePostscriptFile(vector<shared_ptr<Shape>> g){
+    
+    return 0;
+}
