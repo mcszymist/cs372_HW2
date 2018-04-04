1aec7affedd38395433c355d32d92e8d9a608c17
DuaneShaffer <dlshaffer@alaska.edu>
2018-04-04 14:46:48 -0800

    Finished demonstration main.

--- main.cpp
+++ main.cpp
@@ -15,13 +15,13 @@ using std::shared_ptr;
 #include "headers/cps.h"
 // makePostscriptFile
 #include "headers/UniqueShapes.h"
-// 
+// CircleWithArcs
 
 int main () {
     cout << "PS_Translator main" << endl;
     vector<shared_ptr<Shape>> allShapes;
     shared_ptr<Shape> shape(new Triangle(10));
-    shared_ptr<Shape> shape1(new CircleWithArcs(10));
+    shared_ptr<Shape> shape1(new Circle(10));
     shared_ptr<Shape> vertical(new VerticalShape({shape,shape1}));
     shared_ptr<Shape> shape2(new Square(10));
     shared_ptr<Shape> shape3(new Polygon(6,100));
@@ -32,6 +32,10 @@ int main () {
     shared_ptr<Shape> scaled(new Scaled(rotated,1,1));
     scaled->setCursor(200,200);
     allShapes.push_back(scaled);
+    shared_ptr<Shape> unique(new CircleWithArcs(72));
+    shared_ptr<Shape> rotatedUnique(new Rotated(unique, 90));
+    rotatedUnique->setCursor(360,360);
+    allShapes.push_back(rotatedUnique);
     makePostscriptFile(allShapes,"mainPS");
     return 0;
 }
