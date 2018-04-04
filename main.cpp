ef9f63803f300b1cf5071f800052a7ef8ee0ff16
DuaneShaffer <dlshaffer@alaska.edu>
2018-04-04 14:28:59 -0800

    Create a demonstration main to be used for presentation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
-
+#include <vector> 
+using std::vector;
 #include <iostream>
 using std::cout;
 using std::endl;
+#include <memory>
+using std::shared_ptr;
+
+#include "headers/Shape.h"
+// Shape
+#include "headers/ComplexShapes.h" 
+// Rotation
+#include "headers/SimpleShapes.h"
+// Triangle, Circle, Square, Rectangle
+#include "headers/cps.h"
+// makePostscriptFile
+#include "headers/UniqueShapes.h"
+// 
 
 int main () {
     cout << "PS_Translator main" << endl;
+    vector<shared_ptr<Shape>> allShapes;
+    shared_ptr<Shape> shape(new Triangle(10));
+    shared_ptr<Shape> shape1(new CircleWithArcs(10));
+    shared_ptr<Shape> vertical(new VerticalShape({shape,shape1}));
+    shared_ptr<Shape> shape2(new Square(10));
+    shared_ptr<Shape> shape3(new Polygon(6,100));
+    shared_ptr<Shape> shape4(new Rectangle(6,10));
+    shared_ptr<Shape> horizontal(new HorizontalShape({vertical,shape2,shape3,shape4}));
+    Rotation angle(270);
+    shared_ptr<Shape> rotated(new Rotated(horizontal,angle));
+    shared_ptr<Shape> scaled(new Scaled(rotated,1,1));
+    scaled->setCursor(200,200);
+    allShapes.push_back(scaled);
+    makePostscriptFile(allShapes,"mainPS");
     return 0;
 }
